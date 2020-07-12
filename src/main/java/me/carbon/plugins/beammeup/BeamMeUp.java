a2018854cba8981b82a393958e667aedb6734282
Devin Joubert <djoubert@sportingindex.com>
2020-07-12 15:36:56 +0200

    Added config option for location file name

--- src/main/java/me/carbon/plugins/beammeup/BeamMeUp.java
+++ src/main/java/me/carbon/plugins/beammeup/BeamMeUp.java
@@ -2,6 +2,8 @@ package me.carbon.plugins.beammeup;
 
 import me.carbon.plugins.beammeup.commands.BeamCommandManager;
 import me.carbon.plugins.beammeup.locations.LocationManager;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 // TODO: Add some documentation you fool!
@@ -16,7 +18,8 @@ public class BeamMeUp extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        this.locationFileName = "locations.json"; // TODO: Change this so that the user can choose a file
+        this.saveDefaultConfig();
+        this.locationFileName = this.getConfig().getString("location-file-name");
         this.lm = new LocationManager(this);
 
         this.getCommand("beam").setExecutor(new BeamCommandManager(this));
